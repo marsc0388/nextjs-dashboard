13f144bf1f33025a8085d98810b87360b426d557
marsc0388 <marsc0388@gmail.com>
2024-07-16 21:07:00 +0900

    feat: enable partial prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+// segment config
+export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        // incremental allows ppr for specific routes
+        ppr: 'incremental',
+    }
+};
 
 export default nextConfig;
